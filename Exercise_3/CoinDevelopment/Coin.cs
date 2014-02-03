ab0d6c088335ac1c37c6c0f13994cd5a882dcbd5
Chris Barcroft <ChrisABarcroft@gmail.com>
2014-02-03 14:10:15 -0800

    Correcting filestructure

---
+++ Exercise_3/CoinDevelopment/Coin.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinDevelopment
+{
+    class Coin 
+    {
+        public enum Denomination
+        { SLUG = 0, NICKEL = 5, DIME = 10, QUARTER = 25, HALFDOLLAR = 50 } 
+
+        private Denomination _denomination;
+ 
+         // parameterless constructor – coin will be a slug 
+         public Coin() 
+         {
+             _denomination = Denomination.SLUG;
+         }
+ 
+         // parametered constructor – coin will be of appropriate value 
+         // if value passed is outside normal set (e.g. 5 cents = Nickel) 
+         // coin is a slug // <--- I think this description might be for the overload that accepts a Decimal?
+         public Coin(Denomination CoinEnumeral) 
+         {
+             _denomination = CoinEnumeral;
+         }
+ 
+         // This constructor will take a string and return the appropriate enumeral 
+         public Coin(string CoinName)
+         {
+             if (!Enum.TryParse(CoinName, true, out _denomination))
+             {
+                 _denomination = Denomination.SLUG;
+             }
+         }
+ 
+ 
+         // parametered constructor – coin will be of appropriate value 
+         public Coin(decimal CoinValue) 
+         {
+             int coinCents = (int)Decimal.Multiply(CoinValue, 100m);
+
+             if (Enum.IsDefined(typeof(Denomination), coinCents))
+                 _denomination = (Denomination)coinCents;
+             else
+                 _denomination = Denomination.SLUG;
+         }
+ 
+         // This property will get the monetary value of the coin. 
+         public decimal ValueOf
+         {
+             get
+             {
+                 return (decimal)_denomination / 100;
+             }
+         }
+ 
+         // This property will get the coin name as an enumeral 
+         public Denomination CoinEnumeral
+         {
+             get
+             {
+                 return (Denomination)_denomination;
+             }
+         }
+ 
+ 
+         // use Enum.GetName() with a private Denomination instance variable 
+         // to produce a string 
+         public override string ToString()
+         {
+             return Enum.GetName(typeof(Denomination), _denomination);
+         }
+    } 
+}
---
+++ Exercise_3/CoinDevelopment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinDevelopment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test exercises for COIN class.");
+
+            Coin coin_noParams = new Coin();
+            testCoin(coin_noParams, "Parameterless constructor");
+
+            Coin coin_Enumeral = new Coin(Coin.Denomination.DIME);
+            testCoin(coin_Enumeral, "Denomation constructor - Dime");
+
+            Coin coin_String = new Coin("Dime");
+            testCoin(coin_String, "String constructor - Dime");
+
+            Coin coin_Value10 = new Coin(.10m);
+            testCoin(coin_Value10, "Value constructor - .10m");
+
+            Coin coin_Value50 = new Coin(.50m);
+            testCoin(coin_Value50, "Value constructor - .50m");
+
+            Coin coin_badValue = new Coin(.12m);
+            testCoin(coin_badValue, "Invalid Value constructor - .12m");
+
+            Console.WriteLine("ADDITION: Dime + Half Dollar = {0:C}", coin_Value10.ValueOf + coin_Value50.ValueOf);
+            Console.WriteLine("ADDITION: Value-constructed Dime + String-constructed Dime = {0:C}", coin_String.ValueOf + coin_Value10.ValueOf);
+            Console.WriteLine("ADDITION: Denomination-constructed Dime + String-constructed Dime = {0:C}", coin_Enumeral.ValueOf + coin_Value10.ValueOf);
+            Console.WriteLine("ADDITION: Denomination-constructed Dime + Slug = {0:C}", coin_Enumeral.ValueOf + coin_noParams.ValueOf);
+        }
+
+        public static void testCoin(Coin coin, string constructorType) 
+        {
+            Console.WriteLine("Results for: {0}", constructorType);
+            Console.WriteLine("Value of CoinEnumeral: {0}", coin.CoinEnumeral);
+            Console.WriteLine("Value of ValueOf: {0}", coin.ValueOf);
+            Console.WriteLine("Value of ToString(): {0}", coin.ToString());
+            Console.WriteLine();
+        }
+    }
+}
